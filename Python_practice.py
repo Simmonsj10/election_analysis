ff73d99edd84c48164ea84ebde045f23ecc78726
Jerome Simmons <jeromesimmons@Jeromes-MacBook-Pro.local>
2020-12-01 21:11:56 -0800

    test

---
+++ Python_practice.py
@@ -0,0 +1,11 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+counties
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+len(counties_tuple)
+# How many votes did you get?
+my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
